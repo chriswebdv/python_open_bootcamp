6431b5aa84fc4bc37e46d0080a53001434c09c1b
chriswebdv <chriswebdv@gmail.com>
2023-04-11 16:58:15 -0600

    Add main, operaciones files

---
+++ python_module/main.py
@@ -0,0 +1,16 @@
+
+import operaciones 
+
+# Call the functions from operaciones module
+print(operaciones.suma(2, 4))
+print(operaciones.resta(6, 4))
+print(operaciones.multiplica(2, 10))
+print(operaciones.division(2, 78))
+
+
+    
+
+
+
+
+     
---
+++ python_module/operaciones.py
@@ -0,0 +1,20 @@
+# En este ejercicio tendréis que crear un módulo que contenga 
+# las operaciones básicas de una calculadora: sumar, restar, 
+# multiplicar y dividir.
+
+# Este módulo lo importaréis a un archivo python y llamaréis 
+# a las funciones creadas. Los resultados tenéis que mostrarlos 
+# por consola.
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplica(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
