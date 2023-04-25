cb155fee562f53587e9a89367a33d7315264d596
chriswebdv <chriswebdv@gmail.com>
2023-04-25 13:24:54 -0600

    Add test.py myfile.txt

---
+++ module8/test.py
@@ -0,0 +1,27 @@
+# En este ejercicio, tendréis que crear 
+# un archivo py donde creéis un archivo 
+# txt, lo abráis y escribáis dentro del 
+# archivo. Para ello, tendréis que 
+# acceder dos veces al archivo creado.
+
+f = open('myfile.txt', 'a')
+f.write('name1\n')
+f.write('name2\n')
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
